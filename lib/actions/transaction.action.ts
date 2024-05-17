85b2283388756687e11597d507e9e3965e3ed04f
Tamal <tamalkundu2002@gmail.com>
2024-05-17 19:39:51 +0530

    stripe fix

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -1,7 +1,7 @@
-"use server";
+'use server';
 
-import { redirect } from 'next/navigation'
-import Stripe from "stripe";
+import { redirect } from 'next/navigation';
+import Stripe from 'stripe';
 import { handleError } from '../utils';
 import { connectToDatabase } from '../database/mongoose';
 import Transaction from '../database/models/transaction.model';
@@ -16,10 +16,10 @@ export async function checkoutCredits(transaction: CheckoutTransactionParams) {
     line_items: [
       {
         price_data: {
-          currency: 'usd',
+          currency: 'inr',
           unit_amount: amount,
           product_data: {
-            name: transaction.plan,
+            name: transaction.plan
           }
         },
         quantity: 1
@@ -28,14 +28,14 @@ export async function checkoutCredits(transaction: CheckoutTransactionParams) {
     metadata: {
       plan: transaction.plan,
       credits: transaction.credits,
-      buyerId: transaction.buyerId,
+      buyerId: transaction.buyerId
     },
     mode: 'payment',
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-  })
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`
+  });
 
-  redirect(session.url!)
+  redirect(session.url!);
 }
 
 export async function createTransaction(transaction: CreateTransactionParams) {
@@ -44,13 +44,14 @@ export async function createTransaction(transaction: CreateTransactionParams) {
 
     // Create a new transaction with a buyerId
     const newTransaction = await Transaction.create({
-      ...transaction, buyer: transaction.buyerId
-    })
+      ...transaction,
+      buyer: transaction.buyerId
+    });
 
     await updateCredits(transaction.buyerId, transaction.credits);
 
     return JSON.parse(JSON.stringify(newTransaction));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
